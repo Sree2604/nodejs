9badd8c27293d39919c47539594741c01f5226f8
AravindCodesAllDay <aravindsiva2003.in@gmail.com>
2024-02-02 09:41:17 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Customer = require("./models/customer");
+const cors = require("cors");
+
+const app = express();
+mongoose.set("strictQuery", false);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION;
+
+if (!CONNECTION) {
+  console.error();
+  process.exit(1);
+}
+
+app.get("/api/customers", async (req, res) => {
+  try {
+    const result = await Customer.find();
+    res.json({ customers: result });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.get("/api/customers/:id", async (req, res) => {
+  try {
+    const { id: customerId } = req.params;
+    const customer = await Customer.findById(customerId);
+    if (!customer) {
+      res.status(404).json({ error: "User not found" });
+    } else {
+      res.json({ customer });
+    }
+  } catch (e) {
+    res.status(500).json({ error: "something went wrong" });
+  }
+});
+
+app.put("/api/customers/:id", async (req, res) => {
+  try {
+    const customerId = req.params.id;
+    const customer = await Customer.findOneAndReplace(
+      { _id: customerId },
+      req.body,
+      { new: true }
+    );
+    res.json({ customer });
+  } catch (e) {
+    res.status(500).json({ error: "something went wrong" });
+  }
+});
+
+app.patch("/api/customers/:id", async (req, res) => {
+  try {
+    const customerId = req.params.id;
+    const customer = await Customer.findOneAndUpdate(
+      { _id: customerId },
+      req.body,
+      { new: true }
+    );
+    res.json({ customer });
+  } catch (e) {
+    res.status(500).json({ error: "something went wrong" });
+  }
+});
+
+app.delete("/api/customers/:id", async (req, res) => {
+  try {
+    const customerId = req.params.id;
+    const result = await Customer.deleteOne({ _id: customerId });
+    res.json({ deletedCount: result.deletedCount });
+  } catch (e) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.post("/api/customers", async (req, res) => {
+  const customer = new Customer(req.body);
+  try {
+    await customer.save();
+    res.status(201).json({ customer });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
+
+const start = async () => {
+  try {
+    await mongoose.connect(CONNECTION);
+    app.listen(PORT, () => {
+      console.log("App listening on port " + PORT);
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+start();
