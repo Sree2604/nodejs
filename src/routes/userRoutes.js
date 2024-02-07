1492f0ed6960345889d525447bc477b8797d9ec2
Sree2604 <ds04aranganthan@gmail.com>
2024-02-07 14:24:45 +0530

    ...

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,61 +6,61 @@ const mongoose = require('mongoose');
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  try {
-    const { name, mail, phone, pswd } = req.body;
+    try {
+        const { name, mail, phone, pswd } = req.body;
 
-    if (!name || !mail || !phone || !pswd) {
-      return res.status(400).json({
-        message: "All required fields must be provided.",
-      });
+        if (!name || !mail || !phone || !pswd) {
+            return res.status(400).json({
+                message: "All required fields must be provided.",
+            });
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(mail)) {
+            return res.status(400).json({
+                message: "Invalid email format.",
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(pswd, 10);
+
+        const newUser = {
+            name,
+            mail,
+            phone,
+            pswd: hashedPassword,
+        };
+
+        const user = await User.create(newUser);
+
+        return res.status(201).json(user);
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ message: "Internal Server Error" });
     }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(mail)) {
-      return res.status(400).json({
-        message: "Invalid email format.",
-      });
-    }
-
-    const hashedPassword = await bcrypt.hash(pswd, 10);
-
-    const newUser = {
-      name,
-      mail,
-      phone,
-      pswd: hashedPassword,
-    };
-
-    const user = await User.create(newUser);
-
-    return res.status(201).json(user);
-  } catch (error) {
-    console.error(error.message);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
 });
 
 router.get("/:identifier", async (req, res) => {
-  try {
-    const { identifier } = req.params;
-    let user;
+    try {
+        const { identifier } = req.params;
+        let user;
 
-    if (mongoose.Types.ObjectId.isValid(identifier)) {
-      user = await User.findOne({ _id: identifier });
-    } else {
-      user = await User.findOne({ mail: identifier });
+        if (mongoose.Types.ObjectId.isValid(identifier)) {
+            user = await User.findOne({ _id: identifier });
+        } else {
+            user = await User.findOne({ mail: identifier });
+        }
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+
+        return res.status(200).json(user);
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ message: "Internal Server Error" });
     }
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-
-    return res.status(200).json(user);
-  } catch (error) {
-    console.error(error.message);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
 });
 
 router.get('/', async (req, res) => {
@@ -68,89 +68,89 @@ router.get('/', async (req, res) => {
         const users = await User.find({});
 
 
-    return res.status(200).json({
-      count: users.length,
-      data: users,
-    });
-  } catch (error) {
-    console.error(error.message);
-    return res.status(500).json({ message: error.message });
-  }
+        return res.status(200).json({
+            count: users.length,
+            data: users,
+        });
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ message: error.message });
+    }
 });
 
 router.post("/cart", async (req, res) => {
-  try {
-    const { userId, product, quantity } = req.body;
+    try {
+        const { userId, product, quantity } = req.body;
 
-    if (!userId || !product || !quantity) {
-      return res.status(400).json({ message: "Missing required fields" });
+        if (!userId || !product || !quantity) {
+            return res.status(400).json({ message: "Missing required fields" });
+        }
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const existingCartItemIndex = user.cart.findIndex(
+            (item) => item.product === product
+        );
+
+        if (existingCartItemIndex !== -1) {
+            const existingCartItem = user.cart[existingCartItemIndex];
+            console.log("Updating existing item:", existingCartItem);
+
+            existingCartItem.quantity =
+                parseInt(existingCartItem.quantity) + parseInt(quantity);
+            user.cart.splice(existingCartItemIndex, 1);
+            user.cart.push(existingCartItem);
+        } else {
+            const cartItem = new Cart({ product, quantity });
+            user.cart.push(cartItem);
+        }
+
+        await user.save();
+
+        return res
+            .status(200)
+            .json({ message: "Product added to cart successfully" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal Server Error" });
     }
-
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    const existingCartItemIndex = user.cart.findIndex(
-      (item) => item.product === product
-    );
-
-    if (existingCartItemIndex !== -1) {
-      const existingCartItem = user.cart[existingCartItemIndex];
-      console.log("Updating existing item:", existingCartItem);
-
-      existingCartItem.quantity =
-        parseInt(existingCartItem.quantity) + parseInt(quantity);
-      user.cart.splice(existingCartItemIndex, 1);
-      user.cart.push(existingCartItem);
-    } else {
-      const cartItem = new Cart({ product, quantity });
-      user.cart.push(cartItem);
-    }
-
-    await user.save();
-
-    return res
-      .status(200)
-      .json({ message: "Product added to cart successfully" });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
 });
 
 router.delete("/cart", async (req, res) => {
-  try {
-    const { userId, product } = req.body;
+    try {
+        const { userId, product } = req.body;
 
-    if (!userId || !product) {
-      return res.status(400).json({ message: "Missing required fields" });
+        if (!userId || !product) {
+            return res.status(400).json({ message: "Missing required fields" });
+        }
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const existingCartItemIndex = user.cart.findIndex(
+            (item) => item.product === product
+        );
+
+        if (existingCartItemIndex !== -1) {
+            user.cart.splice(existingCartItemIndex, 1);
+        }
+
+        await user.save();
+
+        return res
+            .status(200)
+            .json({ message: "Product deleted from cart successfully" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal Server Error" });
     }
-
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    const existingCartItemIndex = user.cart.findIndex(
-      (item) => item.product === product
-    );
-
-    if (existingCartItemIndex !== -1) {
-      user.cart.splice(existingCartItemIndex, 1);
-    }
-
-    await user.save();
-
-    return res
-      .status(200)
-      .json({ message: "Product deleted from cart successfully" });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
 });
 
 module.exports = router;
